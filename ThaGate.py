18fd5d920d94a8229a39a1e9afbf462bb954c893
Kai Hwang <kai.hwang@gmail.com>
2017-04-10 14:39:13 -0700

    initial commit.

---
+++ ThaGate.py
@@ -0,0 +1,7 @@
+
+from FuncParcel import *
+
+### average matrices
+Morel_Yeo17_M = average_corrmat('/home/despoB/connectome-thalamus/ThaGate/Matrices/NKI*_Morel_plus_Yeo17_645_corrmat', np_txt=True, pickle_object=False)
+Morel_Yeo17_pM = average_corrmat('/home/despoB/connectome-thalamus/ThaGate/Matrices/NKI*_Morel_plus_Yeo17_645_partial_corrmat', np_txt=True, pickle_object=False)
+Morel_Yeo400_M = average_corrmat('/home/despoB/connectome-thalamus/ThaGate/Matrices/NKI*_Morel_plus_Yeo400_645_corrmat', np_txt=True, pickle_object=False)
