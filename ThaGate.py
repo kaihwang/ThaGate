d59c8f94d6806879eaacd7736e094fc269d6d336
Kai Hwang <kai.hwang@gmail.com>
2017-04-12 14:20:49 -0700

    average partial corr.

--- ThaGate.py
+++ ThaGate.py
@@ -1,7 +1,23 @@
 
 from FuncParcel import *
+import numpy as np
 
-### average matrices
+
+### average matrices then find max
 Morel_Yeo17_M = average_corrmat('/home/despoB/connectome-thalamus/ThaGate/Matrices/NKI*_Morel_plus_Yeo17_645_corrmat', np_txt=True, pickle_object=False)
+np.fill_diagonal(Morel_Yeo17_M, 0)
+MaxYeo17_Morel = np.argmax(Morel_Yeo17_M[17:,0:17],1)+1
+
+
 Morel_Yeo17_pM = average_corrmat('/home/despoB/connectome-thalamus/ThaGate/Matrices/NKI*_Morel_plus_Yeo17_645_partial_corrmat', np_txt=True, pickle_object=False)
+np.fill_diagonal(Morel_Yeo17_pM, 0)
+MaxYeo17_Morel_pM = np.argmax(Morel_Yeo17_pM[17:,0:17],1)+1
+
+
 Morel_Yeo400_M = average_corrmat('/home/despoB/connectome-thalamus/ThaGate/Matrices/NKI*_Morel_plus_Yeo400_645_corrmat', np_txt=True, pickle_object=False)
+np.fill_diagonal(Morel_Yeo400_M, 0)
+Morel_Yeo400_M[np.isnan(Morel_Yeo400_M)] = 0
+MaxYeo400_Morel = np.argmax(Morel_Yeo400_M[400:,0:400],1)+1
+
+
+
