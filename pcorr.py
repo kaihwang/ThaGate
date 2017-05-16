f2f3a735efdf0fea39438c9a8062540b7c859d89
Kai Hwang <kai.hwang@gmail.com>
2017-05-16 16:57:37 -0700

    pcorr for thalamic voxel to networks.

--- pcorr.py
+++ pcorr.py
@@ -17,6 +17,24 @@ def function_cal_pcorr_mat(subject , sequence, roi):
 	#np.savetxt(fn, pcorr_mat, fmt='%.4f')
 	np.savetxt(fn, pcorr_mat)
 
+def cal_vox_pcorr_mat(subject , sequence):
+	ts_path = '/home/despoB/kaihwang/Rest/ThaGate/NotBackedUp/NKI_'
+	pcorr_path = '/home/despoB/kaihwang/Rest/ThaGate/Matrices/'
+	fn = ts_path + subject + '__mx_%s_Yeo17Network_TS_000.netts' %(sequence)
+	cortical_roi_ts = np.loadtxt(fn)
+	
+	fn = ts_path + subject + '__mx_%s_Thalamus_indices_TS_000.netts' %(sequence)
+	thalamus_ts = np.loadtxt(fn)
 
-subject, sequence, roi = raw_input().split()
-function_cal_pcorr_mat(subject, sequence, roi)
+	pcorr_mat = pcorr_subcortico_cortical_connectivity(thalamus_ts, cortical_roi_ts)
+	
+	fn = pcorr_path + subject + '_' + 'thavox_plus_Yeo17' + '_' + sequence + '_pcorr_mat'
+	#np.savetxt(fn, pcorr_mat, fmt='%.4f')
+	np.savetxt(fn, pcorr_mat)	
+
+
+# subject, sequence, roi = raw_input().split()
+# function_cal_pcorr_mat(subject, sequence, roi)
+
+subject, sequence = raw_input().split()
+cal_vox_pcorr_mat(subject, sequence)
--- qsub_hcp.sh
+++ qsub_hcp.sh
@@ -12,7 +12,7 @@ cd /home/despoB/kaihwang/Rest/NKI
 for s in $(/bin/ls -d *); do  #$(/bin/ls -d *)
 	#if [ ! -e "/home/despoB/kaihwang/Rest/Graph/gsetCI_${Subject}.mat" ]; then
 	sed "s/s in 0102826_session_1/s in ${s}/g" < ${SCRIPT}/python_qsub.sh > ~/tmp/dFC_graph${s}.sh
-	qsub -l mem_free=13G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/dFC_graph${s}.sh
+	qsub -l mem_free=5G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/dFC_graph${s}.sh
 	#fi
 done
 
